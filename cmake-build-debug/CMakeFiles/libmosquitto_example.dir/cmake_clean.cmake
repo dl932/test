b3e6e00dce949af41859e14bc3d5b0ec81f98d5a
dl932 <dl932@email.arizona.edu>
2020-08-06 18:04:09 -0700

    add

---
+++ cmake-build-debug/CMakeFiles/libmosquitto_example.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/libmosquitto_example.dir/libmosquitto_example.c.o"
+  "libmosquitto_example"
+  "libmosquitto_example.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/libmosquitto_example.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
